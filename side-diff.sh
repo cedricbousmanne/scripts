2a11cbf3ea83d03aaba3d082a9b8c43d77a8ca53
cedric <cedric@studio-sept.be>
2016-02-19 13:00:44 +0100

    update

--- side-diff.sh
+++ side-diff.sh
@@ -2,13 +2,13 @@
 # un-comment one diff tool you'd like to use
 
 # side-by-side diff with custom options:
-# /usr/bin/sdiff -w200 -l "$2" "$5" 
+# /usr/bin/sdiff -w200 -l "$2" "$5"
 
 # using kdiff3 as the side-by-side diff:
 # /usr/bin/kdiff3 "$2" "$5"
 
-# using Meld 
-/usr/bin/meld "$2" "$5"
+# using Meld
+/usr/local/bin/meld "$2" "$5"
 
 # using VIM
 # /usr/bin/vim -d "$2" "$5"
